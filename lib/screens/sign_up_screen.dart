9927b85e7a202687c17b397cc78e70fe7921bdea
Vladislav Adamets <adametsderschopfer@gmail.com>
2024-06-18 23:23:05 +0200

    feat(signup): add indents

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -23,97 +23,114 @@ class _SignUpScreenState extends State<SignUpScreen> {
         body: SafeArea(
             child: Form(
       key: _formKey,
-      child: Column(
-        children: <Widget>[
-          Text("Social Media App",
-              style: Theme.of(context).textTheme.headlineLarge),
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 18),
+                child: Text("Social Media App",
+                    style: Theme.of(context).textTheme.headlineLarge),
+              ),
 
-          // email
-          TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            textInputAction: TextInputAction.next,
-            decoration: const InputDecoration(
-                labelText: "Enter your email",
-                enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black))),
-            onFieldSubmitted: (_) {
-              FocusScope.of(context).requestFocus(_usernameFocusNode);
-            },
-            onSaved: (value) {
-              _email = value!.trim();
-            },
-            validator: (String? value) {
-              if (value!.isEmpty) {
-                return "Please enter your email";
-              }
+              const SizedBox(height: 15),
 
-              // todo: add validator for email -> emailValidationPattern
+              // email
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(
+                    labelText: "Enter your email",
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black))),
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_usernameFocusNode);
+                },
+                onSaved: (value) {
+                  _email = value!.trim();
+                },
+                validator: (String? value) {
+                  if (value!.isEmpty) {
+                    return "Please enter your email";
+                  }
 
-              return null;
-            },
+                  // todo: add validator for email -> emailValidationPattern
+
+                  return null;
+                },
+              ),
+
+              const SizedBox(height: 15),
+
+              // username
+              TextFormField(
+                focusNode: _usernameFocusNode,
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(
+                    labelText: "Enter your username",
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black))),
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_passwordFocusNode);
+                },
+                onSaved: (value) {
+                  _username = value!.trim();
+                },
+                validator: (String? value) {
+                  if (value!.isEmpty) {
+                    return "Please enter your username";
+                  }
+
+                  // todo: add validator for username
+
+                  return null;
+                },
+              ),
+
+              const SizedBox(height: 15),
+
+              // password
+              TextFormField(
+                focusNode: _passwordFocusNode,
+                obscureText: true,
+                decoration: const InputDecoration(
+                    labelText: "Enter your password",
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black))),
+                onFieldSubmitted: (_) {
+                  // TODO: - submit form
+                },
+                onSaved: (value) {
+                  _password = value!.trim();
+                },
+                validator: (String? value) {
+                  if (value!.isEmpty) {
+                    return "Please enter your password";
+                  }
+
+                  if (value.length < 5) {
+                    return "Please enter longer password";
+                  }
+
+                  // todo: add validator for password
+
+                  return null;
+                },
+              ),
+
+              const SizedBox(height: 15),
+
+              TextButton(
+                child: const Text("Sign Up"),
+                onPressed: () {
+                  // TODO: - submit form
+                },
+              )
+            ],
           ),
-
-          // username
-          TextFormField(
-            focusNode: _usernameFocusNode,
-            textInputAction: TextInputAction.next,
-            decoration: const InputDecoration(
-                labelText: "Enter your username",
-                enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black))),
-            onFieldSubmitted: (_) {
-              FocusScope.of(context).requestFocus(_passwordFocusNode);
-            },
-            onSaved: (value) {
-              _username = value!.trim();
-            },
-            validator: (String? value) {
-              if (value!.isEmpty) {
-                return "Please enter your username";
-              }
-
-              // todo: add validator for username
-
-              return null;
-            },
-          ),
-
-          // password
-          TextFormField(
-            focusNode: _passwordFocusNode,
-            obscureText: true,
-            decoration: const InputDecoration(
-                labelText: "Enter your password",
-                enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black))),
-            onFieldSubmitted: (_) {
-              // TODO: - submit form
-            },
-            onSaved: (value) {
-              _password = value!.trim();
-            },
-            validator: (String? value) {
-              if (value!.isEmpty) {
-                return "Please enter your password";
-              }
-
-              if (value.length < 5) {
-                return "Please enter longer password";
-              }
-
-              // todo: add validator for password
-
-              return null;
-            },
-          ),
-
-          TextButton(
-            child: const Text("Sign Up"),
-            onPressed: () {
-              // TODO: - submit form
-            },
-          )
-        ],
+        ),
       ),
     )));
   }
